0f627ccfbdf717a06ec1e8a6f27c0463ce45766d
Wagner R. da Silva <wagaum@gmail.com>
2026-07-02 21:16:21 -0300

    Janela principal

--- gui_tkintertk.py
+++ gui_tkintertk.py
@@ -1,1 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
 
+janela = tk.Tk()
+janela.title("Financiamento de imóveis")
+janela.geometry("400x200") # Largura x altura em pixels
+
+label = ttk.Label(janela, text="Login", font=("Arial", 20) )
+label.pack(pady=20, padx=20)
+texto = tk.Entry(janela, width=20)
+texto.pack(pady=20)
+
+janela.mainloop()
